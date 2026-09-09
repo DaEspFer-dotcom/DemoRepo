0414f14a47959994ca32401020f97c1e8af35a72
DaEspFer-dotcom <daphnee.fer@gmail.com>
2026-09-09 14:32:15 -0400

    Text Change Update

--- Assets/DemoScript.cs
+++ Assets/DemoScript.cs
@@ -1,16 +1,19 @@
+using TMPro;
 using UnityEngine;
 
 public class DemoScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    public TextMeshProUGUI textbox;
 
+    
+    public void OnClick()
+    {
+        textbox.text = "I have changed!";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // public int AdditionTemp(int a, int b)
+    // {
+    //         return a+b;
+    // }
+
 }
